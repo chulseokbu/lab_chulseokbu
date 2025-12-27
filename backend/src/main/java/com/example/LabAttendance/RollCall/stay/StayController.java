e2025114680d4af86a82657ed3269a4b2a2b48a5
tomchccom <dreamkms2014@gmail.com>
2025-12-27 15:00:46 +0900

    chore : 주석 작성 및 import 최적화

--- backend/src/main/java/com/example/LabAttendance/RollCall/stay/StayController.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/stay/StayController.java
@@ -1,12 +1,14 @@
 package com.example.LabAttendance.RollCall.stay;
+
+import com.example.LabAttendance.RollCall.Attendance.AttendanceService;
 import com.example.LabAttendance.RollCall.Attendance.Dto.DailyStayDto;
 import com.example.LabAttendance.RollCall.InOut.Dto.InoutDto;
-
 import com.example.LabAttendance.RollCall.InOut.InOutService;
-import com.example.LabAttendance.RollCall.Attendance.AttendanceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -22,13 +24,14 @@ public class StayController {
      * 최근 7일 체크인/체크아웃 조회
      */
     @GetMapping("/week")
-    public List<InoutDto> getLast7Days(@AuthenticationPrincipal Long memberId) {
+    public List<InoutDto> getLast7Days(
+            @AuthenticationPrincipal Long memberId) {
         return inOutService.getLast7Days(memberId);
     }
 
 
     /**
-     * 최근 30일 간 총 잔류시간 조회
+     * 최근 30일 간 총 잔류시간 조회, 체크인/체크아웃 히스토리를 반환
      */
     @GetMapping("/month")
     public List<DailyStayDto> getLast30Days(
