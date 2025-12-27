0d810e145d2475d078138bca8d28f55cf03da2a4
tomchccom <dreamkms2014@gmail.com>
2025-12-27 15:42:10 +0900

    feat: 상태코드 및 응답 폼 통일

--- backend/src/main/java/com/example/LabAttendance/RollCall/stay/StayController.java
+++ backend/src/main/java/com/example/LabAttendance/RollCall/stay/StayController.java
@@ -4,7 +4,9 @@ import com.example.LabAttendance.RollCall.Attendance.AttendanceService;
 import com.example.LabAttendance.RollCall.Attendance.Dto.DailyStayDto;
 import com.example.LabAttendance.RollCall.InOut.Dto.InoutDto;
 import com.example.LabAttendance.RollCall.InOut.InOutService;
+import com.example.LabAttendance.RollCall.global.ResponneType.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,8 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/lab/stay")
 @RequiredArgsConstructor
+@RequestMapping("/lab/stay")
 public class StayController {
 
     private final InOutService inOutService;
@@ -24,20 +26,27 @@ public class StayController {
      * 최근 7일 체크인/체크아웃 조회
      */
     @GetMapping("/week")
-    public List<InoutDto> getLast7Days(
-            @AuthenticationPrincipal Long memberId) {
-        return inOutService.getLast7Days(memberId);
-    }
-
-
-    /**
-     * 최근 30일 간 총 잔류시간 조회, 체크인/체크아웃 히스토리를 반환
-     */
-    @GetMapping("/month")
-    public List<DailyStayDto> getLast30Days(
+    public ResponseEntity<ApiResponse<List<InoutDto>>> getLast7Days(
             @AuthenticationPrincipal Long memberId
     ) {
-        return attendanceService.getLast30Days(memberId);
+        List<InoutDto> result = inOutService.getLast7Days(memberId);
+
+        return ResponseEntity.ok(
+                ApiResponse.success(result, "최근 7일 체크인/체크아웃 조회 성공")
+        );
+    }
+
+    /**
+     * 최근 30일 간 하루별 총 잔류 시간 조회
+     */
+    @GetMapping("/month")
+    public ResponseEntity<ApiResponse<List<DailyStayDto>>> getLast30Days(
+            @AuthenticationPrincipal Long memberId
+    ) {
+        List<DailyStayDto> result = attendanceService.getLast30Days(memberId);
+
+        return ResponseEntity.ok(
+                ApiResponse.success(result, "최근 30일 잔류 시간 조회 성공")
+        );
     }
 }
-
